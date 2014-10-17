fc730b1220e7b4240c14ffd31d936639e723f907
marlongaines <gainesmarlon@gmail.com>
2014-10-16 17:59:45 -0700

    Update and rename javascript_prototyping.js to car_example.js

--- car_example.js
+++ car_example.js
@@ -6,13 +6,13 @@ function car(color, year, model, make, size) {
     this.size = size;
 }
 
-var Optima = new person("Black", 2014, "Optima", "Kia", "Compact");
-var Liberty = new person("Black", 2014, "Liberty", "Jeep", "Compact SUV");
-var M4 = new person("Black", 2014, "M4", "BMW", "Sport Compact");
-var IS250 = new person("Black", 2009, "IS250", "Lexus", "Sport Compact");
+var Optima = new car("Black", 2014, "Optima", "Kia", "Compact");
+var Liberty = new car("Black", 2014, "Liberty", "Jeep", "Compact SUV");
+var M4 = new car("Black", 2014, "M4", "BMW", "Sport Compact");
+var IS250 = new car("Black", 2009, "IS250", "Lexus", "Sport Compact");
 
 
-document.getElementById("demo").innerHTML = 
+document.getElementById("cars").innerHTML = 
 "<tr><td>" + Optima.make + "</td><td>" + Optima.model + "</td><td>" + Optima.year + "</td></tr>" + "/n" 
 + "<tr><td>" + Liberty.make + "</td><td>" + Liberty.model + "</td><td>" + Liberty.year + "</td></tr>" + "/n"
  + "<tr><td>" + M4.make + "</td><td>" + M4.model + "</td><td>" + M4.year + "</td></tr>" + "/n"
