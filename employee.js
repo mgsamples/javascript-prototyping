262f02666007e33e03fc6b8feb6c1038da14c64b
marlongaines <gainesmarlon@gmail.com>
2014-10-16 18:32:50 -0700

    employee.js

--- employee.js
+++ employee.js
@@ -7,9 +7,9 @@ function employee(first, last, age, citizen, dept) {
 }
 
 
-var NewHire1 = new employee(Bruce, Wayne, 33, US, Applied Sciences);
-var NewHire2 = new employee(Dick, Greyson, 23, US, Management);
-var NewHire3 = new employee(Damian, Wayne, 23, US, Accounting);
+var NewHire1 = new employee("Bruce", "Wayne", 33, "US", "Applied Sciences");
+var NewHire2 = new employee("Dick", "Greyson", 23, "US", "Management");
+var NewHire3 = new employee("Damian", "Wayne", 23, "US", "Accounting");
 
 
 
