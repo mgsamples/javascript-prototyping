4e7a0d86b41fe0498831f636ea15f138a82bfb0b
marlongaines <gainesmarlon@gmail.com>
2014-10-16 18:12:09 -0700

    employee.js

---
+++ employee.js
@@ -0,0 +1,18 @@
+function employee(first, last, age, citizen, dept) {
+    this.first = fist;
+    this.last = last;
+    this.age = age;
+    this.citizen = citizen;
+    this.dept = dept;
+}
+
+
+var NewHire1 = new employee(Bruce, Wayne, 33, US, Applied Sciences);
+var NewHire2 = new employee(Dick, Greyson, 23, US, Management);
+var NewHire3 = new employee(Damian, Wayne, 23, US, Accounting);
+
+
+
+return NewHire1.Last + ', ' + NewHire1.First
+return NewHire2.Last + ', ' + NewHire2.First
+return NewHire3.Last + ', ' + NewHire3.First
